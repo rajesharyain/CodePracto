0cf88cf6e8d63a180c37d55d8e413d13a632e7c4
rajesharyain <rkumar.arya@yahoo.com>
2016-02-24 13:12:11 -0500

    https://github.com/rajesharyain/CodePracto.git Commiting the code at abobe location

---
+++ CodePracto/src/com/codepracto/corejava/abstractClass/SonyLaptop.java
@@ -0,0 +1,11 @@
+package com.codepracto.corejava.abstractClass;
+
+public abstract class SonyLaptop extends Laptop{
+
+	@Override
+	public void printAbstractMethod() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Sony Laptop");
+	}
+
+}
---
+++ CodePracto/src/com/codepracto/corejava/inheritance/OverridingExampleInheritance.java
@@ -0,0 +1,13 @@
+package com.codepracto.corejava.inheritance;
+
+public class OverridingExampleInheritance {
+
+	public void testInstanceMethod(){
+		System.out.println("testInstanceMethod invoked from Supperclass");
+	}
+	
+	public static void testStaticMethod()
+	{
+		System.out.println("testStaticMethod invoked from Supperclass");
+	}
+}
